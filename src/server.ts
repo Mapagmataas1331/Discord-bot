7f7ba222da5c6bddb0d973174463a354248df890
Mapagmataas <98333916+Mapagmataas1331@users.noreply.github.com>
2025-10-17 22:00:26 +0700

    Hot fix

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express, { type Request, type Response } from "express";
+import express, { type Express, type Request, type Response } from "express";
 
-const app = express();
+const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
